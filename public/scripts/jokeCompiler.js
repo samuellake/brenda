e5c85582943809e81556504ce66c88762d3f74d7
Sam Lake <s.lake@net-a-porter.com>
2016-03-10 19:25:30 +0000

    NOGIT

--- public/scripts/jokeCompiler.js
+++ public/scripts/jokeCompiler.js
@@ -3,10 +3,7 @@ var Sentencer = require('./sentencerHelper.js');
 var Gramophone = require('gramophone');
 
 var client = new Twitter({
-    consumer_key: 'IxrJHT3pz3aOc0XKQkETUAlKR',
-    consumer_secret: '6BhKVzjlJQf0OHPwpgughMUJwqG6PIDB48rHLiTWZQeZM0Eq8t',
-    access_token_key: '74590140-rDl27wVTvvB30SIU2zOR3CTAqr0UNalvJ05GKWncd',
-    access_token_secret: '6mP2b2ZYpq2FgM7wI0GVW5VpK9E7i5xu8Kc67xek3lRNo'
+
 });
 module.exports = function(keyword){
 
